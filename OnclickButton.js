38cf0b3cddff26f21736812e89ebfbd481721e09
Gaurav Khandelwal <36410185+gauravk0105@users.noreply.github.com>
2020-08-17 10:53:50 +0530

    Today Commit

--- OnclickButton.js
+++ OnclickButton.js
@@ -11,14 +11,13 @@ function storeDetails() {
     globalThis.username  = nameText.value;
     globalThis.email_address = emailText.value;
     alert("Hello, "+nameText.value+" Your quiz is getting ready....")
-    window.location.href = "main.html"
 }
 
 /* start timer */
 function start_timer() {
     var timer = document.getElementById("timer");
     timer.innerHTML = "Time Left: ";
-    var sec         = 10,
+    var sec         = 1800,
     countDiv    = document.getElementById("div_timer"),
     secpass,
     countDown   = setInterval(function () {
@@ -76,6 +75,106 @@ data = {
           "question":"What does the expression float a = 35 / 0 return?", 
           "options":["0", "Not a Number", "Infinity", "Run time exception"], 
           "answers":["c"]
+      },{
+        "id":6, 
+        "question":"What is the purpose of the radix parameter in the parseInt() method?", 
+        "options":["Numeral system not to be used", "Numeral system to be used", "Conversion mode", "Parsing mode"], 
+        "answers":["b"]
+      },{
+        "id":7, 
+        "question":"What will be the radix value of the parseInt() method when the string begins with 0?", 
+        "options":["6", "7", "8", "9"], 
+        "answers":["c"]
+      },{
+        "id":8, 
+        "question":"What is the return type of the method parseInt()?", 
+        "options":["Date", "Float", "Integer", "Date"], 
+        "answers":["c"]
+      },{
+        "id":9, 
+        "question":"1 kilobyte (kb) =", 
+        "options":["1,024 Bytes", "1,000 Bytes", "1,200 Bytes", "1,275 Bytes"], 
+        "answers":["a"]
+      },{
+        "id":10, 
+        "question":"Which of the below are reserved keyword in Java?", 
+        "options":["array", "goto", "null", "int"], 
+        "answers":["b", "d"]
+      },{
+        "id":11, 
+        "question":"What are the valid statements for static keyword in Java?", 
+        "options":["We can have static block in a class.", "The static block in a class is executed every time an object of class is created.", "We can have static method implementations in interface.", "We can define static block inside a method."], 
+        "answers":["a", "c"]
+      },{
+        "id":12, 
+        "question":" Select all the core concepts of OOPS.", 
+        "options":["Abstraction", "Inheritance", "Interface", "Polymorphism"], 
+        "answers":["a", "b", "d"]
+      },{
+        "id":13, 
+        "question":"Which of the following statements are true for inheritance in Java?", 
+        "options":["The “extend” keyword is used to extend a class in java.", "You can extend multiple classes in java.", "Private members of the superclass are accessible to the subclass.", "We can’t extend Final classes in java."], 
+        "answers":["d"]
+      },{
+        "id":14, 
+        "question":"Which of the below are unchecked exceptions in java?", 
+        "options":["RuntimeException", "ClassCastException", "NullPointerException", "IOException"], 
+        "answers":["a", "b", "c"]
+      },{
+        "id":15, 
+        "question":"What is 6 X 4?", 
+        "options":["22", "24", "28", "32"], 
+        "answers":["b"]
+      },{
+        "id":16, 
+        "question":"The circular wait condition can be prevented by ____________?", 
+        "options":["defining a linear ordering of resource types", "using thread", "using pipes", "all of the mentioned"], 
+        "answers":["a"]
+      },{
+        "id":17, 
+        "question":"To avoid deadlock ____________?", 
+        "options":["there must be a fixed number of resources to allocate", "resource allocation must be done only once", "all deadlocked processes must be aborted", "inversion technique can be used"], 
+        "answers":["a"]
+      },{
+        "id":18, 
+        "question":"In operating system, each process has its own __________?", 
+        "options":["address space and global variables", "open files", "pending alarms, signals and signal handlers", "all of the mentioned"], 
+        "answers":["d"]
+      },{
+        "id":19, 
+        "question":"In Unix, Which system call creates the new process?", 
+        "options":["fork", "create", "new", "none of the mentioned"], 
+        "answers":["a"]
+      },{
+        "id":20, 
+        "question":"In tuple relational calculus P1 → P2 is equivalent to?", 
+        "options":["¬ P1  ∨ P2", "P1 ∨ P2", "P1  ∧ P2", "P1  ∧ ¬P2"], 
+        "answers":["a"]
+      },{
+        "id":21, 
+        "question":"Related fields in a database are grouped to form a?", 
+        "options":["Data file", "Data record", "Menu", "Bank"], 
+        "answers":["b"]
+      },{
+        "id":22, 
+        "question":"The database environment has all of the following components except:", 
+        "options":["Users", "Separate files", "Database", "Database administrator"], 
+        "answers":["b"]
+      },{
+        "id":23, 
+        "question":"A report generator is used to?", 
+        "options":["Update files", "Print files on paper", "Data entry", "Delete files"], 
+        "answers":["b"]
+      },{
+        "id":24, 
+        "question":" A subschema expresses?", 
+        "options":["The logical view", "The physical view", "The external view", "All of the above"], 
+        "answers":["c"]
+      },{
+        "id":25, 
+        "question":"E-R model uses this symbol to represent weak entity set?", 
+        "options":["Dotted rectangle", "Diamond", "Doubly outlined rectangle", "None of these"], 
+        "answers":["c"]
       }
     ]
   }
@@ -84,13 +183,13 @@ data = {
 function createQuiz() {
     var ch = ["a", "b", "c", "d"];
     for (i in data.QA) {
-        var div_count = Math.floor(i/1);
+        var div_count = Math.floor(i/5);
         //console.log(div_count);
         var div_form = document.getElementById("set"+(div_count+1));
         var div_element = document.createElement("div");
         div_element.classList.add("container");
         var heading_h5 = document.createElement("h5");
-        heading_h5.innerHTML="Question"+(data.QA[i].id)+": "+data.QA[i].question;
+        heading_h5.innerHTML="Question "+(data.QA[i].id)+": "+data.QA[i].question;
         heading_h5.classList.add("control-label");
         heading_h5.classList.add("col-sm-10");
         var ul_list = document.createElement("ul");
@@ -128,17 +227,17 @@ function toChecked(flag) {
         if(JSON.stringify(data.QA[i].answers)==JSON.stringify(arr)) correct++;
     }
     sessionStorage.setItem("correct", correct);
-    window.alert("Score: "+correct);
+    window.alert("Your Score: "+correct);
     if(flag==true) {
-        if(parseFloat(correct/5)*100 >= 70)
-        window.alert("Pass and your percentage is: "+(parseFloat(correct/5)*100));
-        else window.alert("Fail and your percentage is: "+(parseFloat(correct/5)*100))
+        if(parseFloat(correct/25)*100 >= 70)
+        window.alert("Pass and your percentage is: "+(parseFloat(correct/25)*100));
+        else window.alert("Fail and your percentage is: "+(parseFloat(correct/25)*100))
         window.location.href='finish.html'
     } else {
         if (confirm('Are you sure you want to end the quiz??')) {
-            if(parseFloat(correct/5)*100 >= 70)
-              alert("Pass and your percentage is: "+(parseFloat(correct/5)*100));
-           else alert("Fail and your percentage is: "+(parseFloat(correct/5)*100))
+            if(parseFloat(correct/25)*100 >= 70)
+              alert("Pass and your percentage is: "+(parseFloat(correct/25)*100));
+           else alert("Fail and your percentage is: "+(parseFloat(correct/25)*100))
            var anchor = document.getElementById("anchor");
            anchor.href="finish.html";
         } //else alert("No Contine it")
